531d73a2378c039f5c6c70f3bb17bd5e6419bdcd
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-06-01 20:21:46 +0300

    added 'feedback'

--- src/common/modal/Modal.tsx
+++ src/common/modal/Modal.tsx
@@ -4,14 +4,16 @@ import s from './Modal.module.scss';
 type PropsType = {
     closeModal: () => void
     children: React.ReactNode
+    title?: string
 }
 
-export const Modal = ({closeModal, children}: PropsType) => {
+export const Modal = ({closeModal, children, title}: PropsType) => {
 
     return (
         <div className={s.modal}>
             <div className={s.overlay} onClick={closeModal}/>
             <div className={s.dialog}>
+                {title && <h3>{title}</h3>}
                 <div className={s.content}>{children}</div>
                 <button className={s.close} onClick={closeModal}>x</button>
             </div>
