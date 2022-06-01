49205681a1a201ebd130cb6a92a40093366547cf
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-06-01 23:45:37 +0300

    added base url heroku

--- src/common/api/portfolio-api.ts
+++ src/common/api/portfolio-api.ts
@@ -2,13 +2,14 @@ import axios from 'axios'
 import {FormikType} from '../../contacts/Contacts';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3010/',
+    //baseURL: 'http://localhost:3010/',
+    baseURL: 'https://portfolio-gmail-smtp.herokuapp.com/'
 })
 
 // api
 export const portfolioAPI = {
-    sendMessage(values:FormikType) {
-         return  instance.post('sendMessage/', values);
+    sendMessage(values: FormikType) {
+        return instance.post('sendMessage/', values);
     }
 }
 
