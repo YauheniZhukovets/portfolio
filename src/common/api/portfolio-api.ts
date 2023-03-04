1e8752968a5e2b3a951eea64ddee0958bab4dd82
YauheniZhukovets <stewart-8@mail.ru>
2023-03-04 21:56:30 +0400

    Edited: instance axios (base url)

--- src/common/api/portfolio-api.ts
+++ src/common/api/portfolio-api.ts
@@ -3,13 +3,13 @@ import {FormikType} from '../../contacts/Contacts';
 
 const instance = axios.create({
     //baseURL: 'http://localhost:3010/',
-    baseURL: 'https://portfolio-gmail-smtp.herokuapp.com/'
+    baseURL: 'https://portfolio-gmail-smtp-sage.vercel.app/'
 })
 
 // api
 export const portfolioAPI = {
     sendMessage(values: FormikType) {
-        return instance.post('sendMessage/', values);
+        return instance.post('sendMessage', values);
     }
 }
 
